7bcf32b5febdb7794219a80994c006262d41afad
EdeerJ <u20201b780@upc.edu.pe>
2023-10-17 17:42:53 -0500

    Eliminacion de codigo extra e inicio del login

--- src/app/component/landing-page/landing-page.component.ts
+++ src/app/component/landing-page/landing-page.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  usuario = '';
+  password = '';
+  mostrarForm = false;
+
+  mostrarLogin() {
+    this.mostrarForm = true;
+  }
+
+  login() {
+    if(this.usuario === 'jadid' && this.password === '1234') {
+      this.router.navigate(['/clientes']);
+    }
+  }
+
+}
